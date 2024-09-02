7fa0aa966c8a90521889bf62dd8beac8795ac34f
ldotmithu <ldotmithurshan222@gmail.com>
2024-09-03 02:09:45 +0530

    add

---
+++ src/mlProject/components/data_ingestion.py
@@ -0,0 +1,51 @@
+import pandas as pd 
+import os,sys
+from mlProject.logger import logging
+from mlProject.exception import CustomException
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','data.csv')
+    
+class DataIngestion:
+    def __init__(self):
+        data_ingestion_config=DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info('Enter the data ingestion part')
+        try:
+            data=pd.read_csv('research\\stud.csv')
+            logging.info('read the data')
+            
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path))
+            
+            data.to_csv(self.data_ingestion_config.raw_data_path,index=False,header=True)
+            logging.info('Save the main data')
+            
+            logging.info('train_test_split the main data')
+            train_data,test_data=train_test_split(data,test_size=0.2,random_state=42)
+            
+            train_data.to_csv(self.data_ingestion_config.train_data_path,index=False,header=True)
+            
+            test_data.to_csv(self.data_ingestion_config.test_data_path,index=False,header=True)
+            logging.info('Save the train and test data')
+            
+            logging.info('compelate the data Ingestion part')
+            
+            return (self.initiate_data_ingestion.train_data_path,
+                    self.initiate_data_ingestion.test_data_path)
+        
+        except Exception as e:
+            CustomException(e,sys)
+
+if __name__=="__main__ ":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()   
+    
+                
+                
